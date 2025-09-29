7430d53d0c1967f974a5761d5a5ec9a82542ba34
Lexcera <118060773+Lexcera@users.noreply.github.com>
2025-09-29 11:35:26 -0300

    Update pxToRem.ts

--- src/utils/pxToRem.ts
+++ src/utils/pxToRem.ts
@@ -1,7 +1,9 @@
+/**
+Convert Pixel To Rem
+@param pixels - Pixels value to be converted
+@returns The Converted rem Value
+*/
+
 export function pxToRem(pixels: number): string {
   return `${pixels / 16}rem`
-=======
- Convert Pixels to rem
- @param pixels - Pixels value to be converted
- @returns The converted rem value
- */
+}
